d573f983f1736f411eeaadfeccde346bd87dbb81
Clayton Parnell <crpjr@amazon.com>
2024-02-08 14:15:19 -0800

    workflow testing

--- test/test_artifacts/v1/scripts/run_keras_tests.sh
+++ test/test_artifacts/v1/scripts/run_keras_tests.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Ref: https://keras.io/guides/, https://github.com/keras-team/keras-io/tree/master
-
+sudo ln -s /usr/lib/x86_64-linux-gnu/libcuda.so.1 /usr/lib/x86_64-linux-gnu/libcuda.so
+export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
 for file in *.py; do
     if [ "$file" != "transfer_learning.py" ]; then
         # skipping transfer_learning.py because it has 20 epochs and it takes a very long time to execute
